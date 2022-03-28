533d721cabd480a4c1d20a13948c131380df3c13
Manh Tran <tienmanh.tran68@gmail.com>
2022-03-28 17:19:48 +0700

    A simple event loop explanation

---
+++ writeJSinWeb/index.js
@@ -0,0 +1,19 @@
+const bar = () => console.log("bar");
+
+const baz = () => console.log("baz");
+
+// const foo = () => {
+//   console.log("foo");
+//   bar();
+//   baz();
+// };
+
+// foo();
+
+const foo = () => {
+  console.log("foo");
+  setTimeout(bar, 0);
+  baz();
+};
+
+foo();
