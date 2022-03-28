f8919b45aa387f6b3026b8ce6ad1a65058b1be2d
Manh Tran <tienmanh.tran68@gmail.com>
2022-03-28 17:29:23 +0700

    Message Queue and Job Queue

--- writeJSinWeb/index.js
+++ writeJSinWeb/index.js
@@ -2,6 +2,9 @@ const bar = () => console.log("bar");
 
 const baz = () => console.log("baz");
 
+const baz2 = () => console.log("baz2");
+
+// Example 1:
 // const foo = () => {
 //   console.log("foo");
 //   bar();
@@ -10,10 +13,32 @@ const baz = () => console.log("baz");
 
 // foo();
 
+// MESSAGE QUEUE: callback in setTimeout
+// JOB QUEUE: result of and async function (Promise)
+
+// =======================================================
+
+// Example 2:
+// const foo = () => {
+//   console.log("foo");
+//   setTimeout(bar, 0);
+//   baz();
+// };
+
+// foo();
+
+// =======================================================
+// Example 3:
 const foo = () => {
   console.log("foo");
   setTimeout(bar, 0);
+  new Promise((resolve, reject) => {
+    resolve("should be right after baz, before bar");
+  }).then((resolve) => console.log(resolve));
+
   baz();
+
+  baz2();
 };
 
 foo();
